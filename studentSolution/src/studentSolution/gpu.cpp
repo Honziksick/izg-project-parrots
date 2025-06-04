910a7305affd50d7facf1a5f3d160f28f3022d64
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-06-04 22:37:54 +0200

    feat: implemented tasks 0 and 1 (tests 0-10 are passing)
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- studentSolution/src/studentSolution/gpu.cpp
+++ studentSolution/src/studentSolution/gpu.cpp
@@ -1,26 +1,229 @@
 /*!
- * @file
- * @brief This file contains implementation of gpu
+ * @file gpu.cpp
+ * @brief This file contains implementation of GPU functionality.
  *
  * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Jan kalina, xkalinj00@stud.fit.vutbr.cz
  */
 
 #include <studentSolution/gpu.hpp>
 
+//! [student_GPU_run]
+void student_GPU_run(GPUMemory &mem, const CommandBuffer &cb) {
+    for(uint32_t iCommand = 0; iCommand < cb.nofCommands; iCommand++) {
+        CommandType type = cb.commands[iCommand].type;
+        CommandData data = cb.commands[iCommand].data;
+        handleCommand(mem, type, data);
+    }
+} // student_GPU_run()
+//! [student_GPU_run]
 
-//! [student_GPU_run]
-void student_GPU_run(GPUMemory&mem,CommandBuffer const&cb){
-  (void)mem;
-  (void)cb;
-  /// \todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
-  /// Měla by umět zpracovat command buffer, čistit framebuffer a kreslit.<br>
-  /// mem obsahuje paměť grafické karty.
-  /// cb obsahuje command buffer pro zpracování.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
-  ///
-  /// V základu jde o to, že cb obsahuje příkazy, které se musí provést nad pamětí mem.
-  /// Správně fungující grafická karta dobře interpretuje příkazy v cb a správně změní obsah paměti mem.
-}
-//! [student_GPU_run]
+static inline void handleCommand(GPUMemory &memory, const CommandType type, const CommandData &data) {
+    switch(type) {
+        /**********************************************************************/
+        /*                          BINDING commands                          */
+        /**********************************************************************/
+        case CommandType::BIND_FRAMEBUFFER:
+            handleBindFramebufferCommand(memory, data);
+            break;
+
+        case CommandType::BIND_PROGRAM:
+            handleBindProgramCommand(memory, data);
+            break;
+
+        case CommandType::BIND_VERTEXARRAY:
+            handleBindVertexArrayCommand(memory, data);
+            break;
+
+        /**********************************************************************/
+        /*                          SETTING commands                          */
+        /**********************************************************************/
+        case CommandType::BLOCK_WRITES_COMMAND:
+            handleBlockWritesCommand(memory, data);
+            break;
+
+        case CommandType::SET_BACKFACE_CULLING_COMMAND:
+            handleSetBackfaceCullingCommand(memory, data);
+            break;
+
+        case CommandType::SET_FRONT_FACE_COMMAND:
+            handleSetFrontFaceCommand(memory, data);
+            break;
+
+        case CommandType::SET_STENCIL_COMMAND:
+            handleSetStencilCommand(memory, data);
+            break;
+
+        case CommandType::SET_DRAW_ID:
+            handleSetDrawIdCommand(memory, data);
+            break;
+
+        /**********************************************************************/
+        /*                           CLEAR commands                           */
+        /**********************************************************************/
+        case CommandType::CLEAR_COLOR:
+            handleClearColorCommand(memory, data.clearColorCommand);
+            break;
+
+        case CommandType::CLEAR_DEPTH:
+            handleClearDepthCommand(memory, data.clearDepthCommand);
+            break;
+
+        case CommandType::CLEAR_STENCIL:
+            handleClearStencilCommand(memory, data.clearStencilCommand);
+            break;
+
+        /**********************************************************************/
+        /*                           TODO commands                            */
+        /**********************************************************************/
+        case CommandType::USER_COMMAND:     // TODO: implement user command
+        case CommandType::DRAW:             // TODO: implement draw command
+        case CommandType::SUB_COMMAND:      // TODO: implement sub command
+        case CommandType::EMPTY:
+        default:
+            break;
+    } // switch(type)
+} // handleCommand()
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                              COMMAND HANDLERS                              */
+/*                                                                            */
+/******************************************************************************/
+
+static inline void handleBindFramebufferCommand(GPUMemory &memory, const CommandData &data) {
+    memory.activatedFramebuffer = data.bindFramebufferCommand.id;
+} // handleBindFramebufferCommand()
+
+static inline void handleBindProgramCommand(GPUMemory &memory, const CommandData &data) {
+    memory.activatedProgram = data.bindProgramCommand.id;
+} // handleBindProgramCommand()
+
+static inline void handleBindVertexArrayCommand(GPUMemory &memory, const CommandData &data) {
+    memory.activatedVertexArray = data.bindVertexArrayCommand.id;
+} // handleBindVertexArrayCommand()
+
+static inline void handleBlockWritesCommand(GPUMemory &memory, const CommandData &data) {
+    memory.blockWrites = data.blockWritesCommand.blockWrites;
+} // handleBlockWritesCommand()
+
+static inline void handleSetBackfaceCullingCommand(GPUMemory &memory, const CommandData &data) {
+    memory.backfaceCulling.enabled = data.setBackfaceCullingCommand.enabled;
+} // handleSetBackfaceCullingCommand()
+
+static inline void handleSetFrontFaceCommand(GPUMemory &memory, const CommandData &data) {
+    memory.backfaceCulling.frontFaceIsCounterClockWise = data.setFrontFaceCommand.frontFaceIsCounterClockWise;
+} // handleSetFrontFaceCommand()
+
+static inline void handleSetStencilCommand(GPUMemory &memory, const CommandData &data) {
+    memory.stencilSettings = data.setStencilCommand.settings;
+} // handleSetStencilCommand()
+
+static inline void handleSetDrawIdCommand(GPUMemory &memory, const CommandData &data) {
+    memory.gl_DrawID = data.setDrawIdCommand.id;
+} // handleSetDrawIdCommand()
+
+static inline void handleClearColorCommand(const GPUMemory &memory, const ClearColorCommand &clearCommand) {
+    const Framebuffer &framebuffer = memory.framebuffers[memory.activatedFramebuffer];
+    const Image &color = framebuffer.color;
+
+    // Check if the color buffer is valid
+    if(!color.data) {
+        return;
+    }
+
+    // Clear the color buffer for each pixel
+    for(uint32_t y = 0; y < framebuffer.height; y++) {
+        for(uint32_t x = 0; x < framebuffer.width; x++) {
+            uint8_t *pixel = getPixelMaybeReversed(color, x, y, framebuffer.height, framebuffer.yReversed);
+
+            // Clear the pixel
+            for(uint32_t iChannel = 0; iChannel < color.channels; iChannel++) {
+                const float channelComponent = pickChannel(clearCommand.value, color.channelTypes[iChannel]);
+
+                // Clear each channel (R, G, B, A) based on the image format
+                if(color.format == Image::U8) {
+                    pixel[iChannel] = castNormalizedFloatToUnsignedInt8(channelComponent);
+                }
+                else {
+                    reinterpret_cast<float*>(pixel)[iChannel] = channelComponent;
+                }
+            } // for(c)
+        } // for(x)
+    } // for(y)
+} // handleClearColorCommand()
+
+static inline void handleClearDepthCommand(const GPUMemory &memory, const ClearDepthCommand &clearCommand) {
+    const Framebuffer &framebuffer = memory.framebuffers[memory.activatedFramebuffer];
+    const Image &depth = framebuffer.depth;
+
+    // Check if the depth buffer is valid
+    if(!depth.data) {
+        return;
+    }
+
+    // Clear the depth buffer for each pixel
+    for(uint32_t y = 0; y < framebuffer.height; y++) {
+        for(uint32_t x = 0; x < framebuffer.width; x++) {
+            uint8_t *pixel = getPixelMaybeReversed(depth, x, y, framebuffer.height, framebuffer.yReversed);
+
+            // Stencil is always 1 channel (hence the [0]) float value
+            reinterpret_cast<float*>(pixel)[0] = clearCommand.value;
+        }
+    }
+} // handleClearDepthCommand()
+
+static inline void handleClearStencilCommand(const GPUMemory &memory, const ClearStencilCommand &clearCommand) {
+    const Framebuffer &framebuffer = memory.framebuffers[memory.activatedFramebuffer];
+    const Image &stencil = framebuffer.stencil;
+
+    // Check if the stencil buffer is valid
+    if(!stencil.data) {
+        return;
+    }
+
+    // Clear the stencil buffer for each pixel
+    for(uint32_t y = 0; y < framebuffer.height; y++) {
+        for(uint32_t x = 0; x < framebuffer.width; x++) {
+            uint8_t *pixel = getPixelMaybeReversed(stencil, x, y, framebuffer.height, framebuffer.yReversed);
+
+            // Stencil is always 1 channel (hence the [0]) uint8_t value
+            pixel[0] = clearCommand.value;
+        }
+    }
+} // handleClearStencilCommand()
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                              HELPER FUNCTIONS                              */
+/*                                                                            */
+/******************************************************************************/
+
+static inline uint8_t *getPixelMaybeReversed(const Image &img, const uint32_t x, const uint32_t y,
+                                             const uint32_t height, const bool yReversed) {
+    const uint32_t maybeReversedY = yReversed ? (height - y - 1) : y;
+    return static_cast<uint8_t*>(img.data) + (maybeReversedY * img.pitch) + (x * img.bytesPerPixel);
+} // getPixelMaybeReversed()
+
+static inline uint8_t castNormalizedFloatToUnsignedInt8(const float value) {
+    return static_cast<uint8_t>(glm::clamp(value, 0.f, 1.f) * 255.f + 0.5f);
+} // castNormalizedFloatToUnsignedInt8()
+
+static inline float pickChannel(const glm::vec4 &color, const Image::Channel channel) {
+    switch(channel) {
+        case Image::RED:
+            return color.r;
+        case Image::GREEN:
+            return color.g;
+        case Image::BLUE:
+            return color.b;
+        case Image::ALPHA:
+            return color.a;
+        default:
+            return 0.f;
+    } // switch(channel)
+} // pickChannel()
 
 /*** end of file gpu.cpp ***/
--- studentSolution/src/studentSolution/gpu.hpp
+++ studentSolution/src/studentSolution/gpu.hpp
@@ -1,21 +1,173 @@
 /*!
- * @file
+ * @file gpu.hpp
  * @brief This file contains class that represents graphic card.
  *
  * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Jan kalina, xkalinj00@stud.fit.vutbr.cz
  */
 #pragma once
 
 #include <solutionInterface/gpu.hpp>
 
 /**
- * @brief function that executes work stored in command buffer on the gpu memory.
- * This function represents the functionality of GPU.
- * It can render stuff, it can clear framebuffer.
- * It can process work stored in command buffer
+ * @brief Function that executes work stored in command buffer on the gpu memory.
+ * @details This function represents the functionality of GPU. It can render
+ *          stuff, it can clear framebuffer. It can process work stored in
+ *          command buffer
  *
- * @param mem gpu memory
- * @param cb command buffer - packaged of work sent to the gpu
+ * @param mem GPU memory.
+ * @param cb Command buffer - packaged of work sent to the GPU.
  */
-void student_GPU_run(GPUMemory&mem,CommandBuffer const&cb);
+void student_GPU_run(GPUMemory &mem, const CommandBuffer &cb);
 
+/**
+ * @brief Processes a single GPU command.
+ *
+ * @details This function handles execution of a specific GPU command based
+ *          on its type. It interprets the command data and performs the
+ *          corresponding operation on GPU memory.
+ *
+ * @param memory Reference to the GPU memory where operations will be performed.
+ * @param type The type of command to be executed.
+ * @param data The data associated with the command to be processed.
+ */
+void handleCommand(GPUMemory &memory, CommandType type, const CommandData &data);
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                              COMMAND HANDLERS                              */
+/*                                                                            */
+/******************************************************************************/
+
+/**
+ * @brief Handles binding a framebuffer to GPU memory.
+ *
+ * @param memory Reference to GPU memory where the framebuffer will be bound.
+ * @param data Command data containing the framebuffer ID to bind.
+ */
+void handleBindFramebufferCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles binding a shader program to GPU memory.
+ *
+ * @param memory Reference to GPU memory where the program will be bound.
+ * @param data Command data containing the program ID to bind.
+ */
+void handleBindProgramCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles binding a vertex array to GPU memory.
+ *
+ * @param memory Reference to GPU memory where the vertex array will be bound.
+ * @param data Command data containing the vertex array ID to bind.
+ */
+void handleBindVertexArrayCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles enabling/disabling write operations to framebuffer.
+ *
+ * @param memory Reference to GPU memory where write blocking will be configured.
+ * @param data Command data containing block writes flag.
+ */
+void handleBlockWritesCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles enabling/disabling backface culling.
+ *
+ * @param memory Reference to GPU memory where backface culling will be configured.
+ * @param data Command data containing backface culling parameters.
+ */
+void handleSetBackfaceCullingCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles setting front face winding direction.
+ *
+ * @param memory Reference to GPU memory where front face direction will be set.
+ * @param data Command data containing front face winding direction.
+ */
+void handleSetFrontFaceCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles configuring stencil test parameters.
+ *
+ * @param memory Reference to GPU memory where stencil settings will be updated.
+ * @param data Command data containing stencil test parameters.
+ */
+void handleSetStencilCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles setting the draw ID for instanced rendering.
+ *
+ * @param memory Reference to GPU memory where draw ID will be set.
+ * @param data Command data containing draw ID value.
+ */
+void handleSetDrawIdCommand(GPUMemory &memory, const CommandData &data);
+
+/**
+ * @brief Handles clearing the color buffer with specified color.
+ *
+ * @param memory Reference to GPU memory containing the framebuffer.
+ * @param clearCommand Command data containing the clear color value.
+ */
+void handleClearColorCommand(const GPUMemory &memory, const ClearColorCommand &clearCommand);
+
+/**
+ * @brief Handles clearing the depth buffer with specified value.
+ *
+ * @param memory Reference to GPU memory containing the framebuffer.
+ * @param clearCommand Command data containing the clear depth value.
+ */
+void handleClearDepthCommand(const GPUMemory &memory, const ClearDepthCommand &clearCommand);
+
+/**
+ * @brief Handles clearing the stencil buffer with specified value.
+ *
+ * @param memory Reference to GPU memory containing the framebuffer.
+ * @param clearCommand Command data containing the clear stencil value.
+ */
+void handleClearStencilCommand(const GPUMemory &memory, const ClearStencilCommand &clearCommand);
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                              HELPER FUNCTIONS                              */
+/*                                                                            */
+/******************************************************************************/
+
+/**
+ * @brief Gets a pointer to a pixel in an image, handling y-coordinate
+ *        reversal if needed.
+ *
+ * @param img Reference to the image.
+ * @param x X-coordinate of the pixel.
+ * @param y Y-coordinate of the pixel.
+ * @param height Height of the image.
+ * @param yReversed Flag indicating if y-coordinates are stored in reverse order.
+ *
+ * @return `uint8_t*` Pointer to the specified pixel data.
+ */
+uint8_t *getPixelMaybeReversed(const Image &img, uint32_t x, uint32_t y,
+                               uint32_t height, bool yReversed);
+
+/**
+ * @brief Converts a normalized float value [0.0, 1.0] to an unsigned 8-bit
+ *        integer [0, 255].
+ *
+ * @param value Float value to convert (expected to be in range [0.0, 1.0]).
+ *
+ * @return `uint8_t` Converted 8-bit unsigned integer value.
+ */
+uint8_t castNormalizedFloatToUnsignedInt8(float value);
+
+/**
+ * @brief Extracts a specific color channel from a color vector.
+ *
+ * @param color RGBA color vector.
+ * @param channel The color channel to extract (R, G, B, or A).
+ *
+ * @return `float` The value of the specified color channel.
+ */
+float pickChannel(const glm::vec4 &color, Image::Channel channel);
+
+/*** end of file gpu.hpp ***/
