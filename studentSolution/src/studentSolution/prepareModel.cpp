f78dde6abbbe58b0325210c1c2cdc9a37711a214
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-06-08 23:27:10 +0200

    feat: implementation of `prepareModel.cpp` (all test passing)
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- studentSolution/src/studentSolution/prepareModel.cpp
+++ studentSolution/src/studentSolution/prepareModel.cpp
@@ -1,68 +1,261 @@
 /*!
- * @file
- * @brief This file contains functions for model rendering
+ * @file prepareModel.cpp
+ * @brief This file contains functions for model rendering.
  *
  * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Jan kalina, xkalinj00@stud.fit.vutbr.cz
  */
+
 #include <studentSolution/prepareModel.hpp>
 #include <studentSolution/gpu.hpp>
+#include <studentSolution/shaderFunctions.hpp>
 #include <solutionInterface/uniformLocations.hpp>
 
-
-/**
- * @brief This function prepares model into memory and creates command buffer
- *
- * @param mem gpu memory
- * @param commandBuffer command buffer
- * @param model model structure
+/*
+ * When implementing this part of the project, I maximally based my code on the
+ * assignment description, graphics, hints and pseudo-code provided. I also tried
+ * to comment on my thoughts and decisions so it is evident that I understand
+ * the problematics even though I used the pseudo-code as a base for my
+ * implementation. I'd like to thank doctor Tomáš Milet for his great work
+ * on the assignment and project skeleton. I only wish more assignments were
+ * like this one ;-).
  */
+
+/******************************************************************************/
+/*                                                                            */
+/*                    PROTOTYPES of used custom functions                     */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef XKALINJ00_PREPARE_MODEL_SOLUTION
+#define XKALINJ00_PREPARE_MODEL_SOLUTION
+
+void prepareNode(GPUMemory &memory, CommandBuffer &commandBuffer, const Model &model,
+                 const Node &node, const glm::mat4 &parentMatrix, uint32_t &drawCounter,
+                 uint32_t &vertexArrayCounter);
+
+#endif // XKALINJ00_PREPARE_MODEL_SOLUTION
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                PREPARE MODEL strongly based on pseudo-code                 */
+/*                                                                            */
+/******************************************************************************/
+
 //! [drawModel]
-void student_prepareModel(GPUMemory&mem,CommandBuffer&commandBuffer,Model const&model){
-  (void)mem;
-  (void)commandBuffer;
-  (void)model;
-  /// \todo Tato funkce připraví command buffer pro model a nastaví správně pamět grafické karty.<br>
-  /// Vaším úkolem je správně projít model a vložit vykreslovací příkazy do commandBufferu.
-  /// Zároveň musíte vložit do paměti textury, buffery a uniformní proměnné, které buffer command buffer využívat.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace a v testech.
-}
+void student_prepareModel(GPUMemory &mem, CommandBuffer &commandBuffer, const Model &model) {
+    // Copy buffers: mem.buffers  = ...;
+    for(size_t iBuffer = 0; iBuffer < model.nofBuffers; iBuffer++) {
+        mem.buffers[iBuffer] = model.buffers[iBuffer];
+    }
+
+    // Copy textures: mem.textures = ...;
+    for(size_t iTexture = 0; iTexture < model.nofTextures; iTexture++) {
+        mem.textures[iTexture] = model.textures[iTexture];
+    }
+
+    uint32_t vertexArrayCounter{0};  // index into 'vertexArrays'
+    uint32_t drawCounter{0};         // draw commands IDs
+
+    // Recursively prepare nodes using pre-order traversal
+    constexpr auto identityMatrix = glm::mat4(1.f);
+    for(size_t iRoot = 0; iRoot < model.nofRoots; iRoot++) {
+        prepareNode(mem, commandBuffer, model, model.roots[iRoot],
+                    identityMatrix, drawCounter, vertexArrayCounter);
+    }
+} // student_prepareModel()
 //! [drawModel]
 
-/**
- * @brief This function represents vertex shader of texture rendering method.
- *
- * @param outVertex output vertex
- * @param inVertex input vertex
- * @param si shader interface
- */
+inline void prepareNode(GPUMemory &memory, CommandBuffer &commandBuffer, const Model &model,
+                        const Node &node, const glm::mat4 &parentMatrix, uint32_t &drawCounter,
+                        uint32_t &vertexArrayCounter) {
+    // Calculate the model matrix by combining parent and local transformations
+    const glm::mat4 modelMatrix = parentMatrix * node.modelMatrix;
+
+    // Process the node only if it has a valid mesh
+    if(node.mesh >= 0) {
+        // Get mesh data from the model
+        const Mesh &mesh = model.meshes[node.mesh];
+
+        // Create and initialize vertex array
+        VertexArray vertexArray{};
+        vertexArray.indexBufferID = mesh.indexBufferID;
+        vertexArray.indexOffset = mesh.indexOffset;
+        vertexArray.indexType = mesh.indexType;
+        vertexArray.vertexAttrib[0] = mesh.position;
+        vertexArray.vertexAttrib[1] = mesh.normal;
+        vertexArray.vertexAttrib[2] = mesh.texCoord;
+
+        // Save the new vertex array in the GPU memory
+        memory.vertexArrays[vertexArrayCounter] = vertexArray;
+
+        // Bind the vertex array for the current draw call
+        pushBindVertexArrayCommand(commandBuffer, vertexArrayCounter);
+
+        // Configure backface culling (disable for double-sided materials)
+        pushSetBackfaceCullingCommand(commandBuffer, !mesh.doubleSided);
+
+        // Create draw command with the number of indices to render
+        pushDrawCommand(commandBuffer, mesh.nofIndices);
+
+        // Write uniform data to memory
+        memory.uniforms[getUniformLocation(drawCounter, MODEL_MATRIX)].m4 = modelMatrix;
+        memory.uniforms[getUniformLocation(drawCounter, INVERSE_TRANSPOSE_MODEL_MATRIX)].m4 = glm::transpose(glm::inverse(modelMatrix));
+        memory.uniforms[getUniformLocation(drawCounter, DIFFUSE_COLOR)].v4 = mesh.diffuseColor;
+        memory.uniforms[getUniformLocation(drawCounter, TEXTURE_ID)].i1 = mesh.diffuseTexture;
+        memory.uniforms[getUniformLocation(drawCounter, DOUBLE_SIDED)].v1 = mesh.doubleSided ? 1.f : 0.f;
+
+        // Increment counters for the next draw call
+        vertexArrayCounter++;
+        drawCounter++;
+    } // if(node.mesh >= 0)
+
+    // Recursively prepare nodes using pre-order traversal
+    for(size_t iChild = 0; iChild < node.nofChildren; iChild++) {
+        prepareNode(memory, commandBuffer, model, node.children[iChild],
+                    modelMatrix, drawCounter, vertexArrayCounter);
+    } // for(iChild)
+} // prepareNode()
+
+
+/******************************************************************************/
+/*                                                                            */
+/*       VERTEX SHADER strongly based on description in the assignment,       */
+/*                   which I followed almost to the letter                    */
+/*                                                                            */
+/******************************************************************************/
+
 //! [drawModel_vs]
-void student_drawModel_vertexShader(OutVertex&outVertex,InVertex const&inVertex,ShaderInterface const&si){
-  (void)outVertex;
-  (void)inVertex;
-  (void)si;
-  /// \todo Tato funkce reprezentujte vertex shader.<br>
-  /// Vaším úkolem je správně trasnformovat vrcholy modelu.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
-}
+void student_drawModel_vertexShader(OutVertex &outVertex, const InVertex &inVertex, const ShaderInterface &si) {
+    // The input is vertices that have position (3f), normal (3f), and
+    // texturing coordinates (2f) (attributes 0, 1, and 2)
+    const glm::vec3 modelSpaceVertexPosition = inVertex.attributes[0].v3;  // vertex position in model-space
+    const glm::vec3 modelSpaceVertexNormal = inVertex.attributes[1].v3;    // vertex normal in model-space
+    const glm::vec2 texturingCoordinates = inVertex.attributes[2].v2;      // texture coordinates
+
+    // Uniform variables include the projectionView matrix, the model matrix, and the inverse transpose matrix
+    const glm::mat4 cameraProjectionViewMatrix = si.uniforms[getUniformLocation(si.gl_DrawID, PROJECTION_VIEW_MATRIX)].m4;           // cameraProjectionView camera projection and view matrix
+    const glm::mat4 lightProjectionViewMatrix = si.uniforms[getUniformLocation(si.gl_DrawID, USE_SHADOW_MAP_MATRIX)].m4;             // lightProjectionView light projection and view matrix - for shadows
+    const glm::mat4 modelMatrix = si.uniforms[getUniformLocation(si.gl_DrawID, MODEL_MATRIX)].m4;                                    // model matrix
+    const glm::mat4 inverseTransposeModelMatrix = si.uniforms[getUniformLocation(si.gl_DrawID, INVERSE_TRANSPOSE_MODEL_MATRIX)].m4;  // inverse transpose matrix
+
+    // Transform vertex and normal to world space "m*glm::vec4(pos,1.f)"
+    const auto worldSpaceVertexPosition = glm::vec3(modelMatrix * glm::vec4(modelSpaceVertexPosition, 1.f));
+
+    // Transform normal to world space "itm*glm::vec4(nor,0.f)"
+    const auto worldSpaceVertexNormal = glm::vec3(inverseTransposeModelMatrix * glm::vec4(modelSpaceVertexNormal, 0.f));
+
+    // The output is vertices that have a position (3f) and normal (3f) in world space,
+    // texturing coordinates (2f) and vertex position in light clip-space (4f) (attributes 0, 1, 2, 3).
+    outVertex.attributes[0].v3 = worldSpaceVertexPosition;  // vertex position in world-space
+    outVertex.attributes[1].v3 = worldSpaceVertexNormal;    // vertex normal in world-space
+    outVertex.attributes[2].v2 = texturingCoordinates;      // texture coordinates
+    outVertex.attributes[3].v4 = lightProjectionViewMatrix * glm::vec4(worldSpaceVertexPosition, 1.f);  // vertex position in clip-space of the light
+
+    // Transform position to camera clip space "cameraProjectionView*model*pos" (worldSpaceVertexPosition = model * pos)
+    outVertex.gl_Position = cameraProjectionViewMatrix * glm::vec4(worldSpaceVertexPosition, 1.f);
+} // student_drawModel_vertexShader()
 //! [drawModel_vs]
 
-#include<iostream>
 
-/**
- * @brief This functionrepresents fragment shader of texture rendering method.
- *
- * @param outFragment output fragment
- * @param inFragment input fragment
- * @param si shader interface
- */
+/******************************************************************************/
+/*                                                                            */
+/*      FRAGMENT SHADER strongly based on description in the assignment,      */
+/*                   which I followed almost to the letter                    */
+/*                                                                            */
+/******************************************************************************/
+
 //! [drawModel_fs]
-void student_drawModel_fragmentShader(OutFragment&outFragment,InFragment const&inFragment,ShaderInterface const&si){
-  (void)outFragment;
-  (void)inFragment;
-  (void)si;
-  /// \todo Tato funkce reprezentujte fragment shader.<br>
-  /// Vašim úkolem je správně obarvit fragmenty a osvětlit je pomocí lambertova osvětlovacího modelu.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
-}
+void student_drawModel_fragmentShader(OutFragment &outFragment, const InFragment &inFragment, const ShaderInterface &si) {
+    // The input is fragments that have: position (3f), normal (3f), texturing coordinates (2f) and
+    // position in clip-space of the light space for reading from the shadow map (attributes 0,1,2,3)
+    const glm::vec3 worldSpaceFragmentPosition = inFragment.attributes[0].v3;  // fragment position in world-space
+    glm::vec3 worldSpaceFragmentNormal = inFragment.attributes[1].v3;          // fragment normal in world-space
+    const glm::vec2 texturingCoordinates = inFragment.attributes[2].v2;        // texture coordinates
+    glm::vec4 clipSpaceShadowPosition = inFragment.attributes[3].v4;           /* fragment position in clip-space of light for
+                                                                                  shadow map addressing and shadow calculation */
+
+    // Uniform variables include light position (3f), camera position (3f),
+    // diffuse color (4f), texture number (1i), and doubleSided flag (1f)
+    const glm::vec3 lightPosition = si.uniforms[getUniformLocation(si.gl_DrawID, LIGHT_POSITION)].v3;          // light position in world-space
+    const glm::vec3 cameraPosition = si.uniforms[getUniformLocation(si.gl_DrawID, CAMERA_POSITION)].v3;        // camera position in world-space
+    const int32_t shadowMapId = si.uniforms[getUniformLocation(si.gl_DrawID, SHADOWMAP_ID)].i1;                // texture number that contains the shadow map, or -1 if there are no shadows
+    const glm::vec3 ambientLightColor = si.uniforms[getUniformLocation(si.gl_DrawID, AMBIENT_LIGHT_COLOR)].v3; // ambient light color
+    const glm::vec3 lightColor = si.uniforms[getUniformLocation(si.gl_DrawID, LIGHT_COLOR)].v3;                // light color
+    glm::vec4 diffuseColor = si.uniforms[getUniformLocation(si.gl_DrawID, DIFFUSE_COLOR)].v4;                  // diffuse color
+    const int32_t textureId = si.uniforms[getUniformLocation(si.gl_DrawID, TEXTURE_ID)].i1;                    // texture number or -1 if there are no textures
+    const float isDoubleSided = si.uniforms[getUniformLocation(si.gl_DrawID, DOUBLE_SIDED)].v1;                // doubleSided flag (1.f if it is, 0.f if it is not)
+
+    // Input normal should be normalized with N=glm::normalize(nor)
+    worldSpaceFragmentNormal = glm::normalize(worldSpaceFragmentNormal);
+
+    // The diffuse color of the material is either stored in a uniform variable or in a texture
+    // It depends on whether the texture number is negative or not
+    if(textureId >= 0) {
+        diffuseColor = student_read_textureClamp(si.textures[textureId], texturingCoordinates);
+    } // if(diffuse color is stored in a texture)
+
+    // If the doubleSided flag is set (is > 0), it is a double-sided surface.
+    // In this case, it is necessary to rotate the normal if it is facing away
+    // from the camera (use the camera position in the uniform variable)
+    if(isDoubleSided > 0.f) {
+        const glm::vec3 viewDirection = glm::normalize(cameraPosition - worldSpaceFragmentPosition);
+        const float normalDirection = glm::dot(viewDirection, worldSpaceFragmentNormal);
+
+        // Flip the normal if it is facing away
+        if(normalDirection < 0.f) {
+            worldSpaceFragmentNormal = glm::normalize(-worldSpaceFragmentNormal);
+        }
+    } // if(isDoubleSided)
+
+    // Calculate a Lambert lighting model with shadows using shadow mapping
+    // Note: From IZG lecture presentation: ID = IL * rD * cos(phi), ID = IL * rD * (vec(N) * vec(L)),
+    //       where ID = diffuse component, IL = light intensity, rD = diffusion coefficient of the surface,
+    //       vec(N) = surface normal, vec(L) = vector from the fragment to the light source
+    const glm::vec3 lightDirection = glm::normalize(lightPosition - worldSpaceFragmentPosition);  // normalized vector from the fragment to the light
+
+    // Scalar product of the normal and the light direction (vec(N) * vec(L))
+    const float scalarProductNL = glm::clamp(glm::dot(worldSpaceFragmentNormal, lightDirection), 0.f, 1.f);  // surfaces facing away from the light are not illuminated
+
+    // Ambient component according to the formula IA = Ia * rD
+    const glm::vec3 ambientComponent = ambientLightColor * glm::vec3(diffuseColor);
+
+    // Diffusion component according to the formula ID = IL * rD * (vec(N) * vec(L))
+    const glm::vec3 diffuseComponent = lightColor * glm::vec3(diffuseColor) * scalarProductNL;
+
+    // The resulting color is the sum of the ambient and diffuse components
+    glm::vec3 color = ambientComponent + diffuseComponent;
+
+    // Check if alpha is less than 0.5 and set discard flag if needed
+    if(diffuseColor.a < 0.5f) {
+        outFragment.discard = true;
+        return;  // Discarding so color does not matter
+    }
+
+    // Calculate whether the fragment is in shadow
+    // You need to read the depth from the shadow map and compare it to the distance to the light
+    if(shadowMapId >= 0) {
+        // Perspective division - convert from clip-space to NDC
+        const glm::vec3 shadowPosition = glm::vec3(clipSpaceShadowPosition) / clipSpaceShadowPosition.w;
+
+        // Check if fragment is outside shadow map bounds (not in shadow)
+        if(shadowPosition.x >= 0.0f && shadowPosition.x <= 1.0f &&
+            shadowPosition.y >= 0.0f && shadowPosition.y <= 1.0f) {
+            // Read depth from shadow map
+            auto depthFromShadowMap = student_read_textureClamp(si.textures[shadowMapId], glm::vec2(shadowPosition)).r;
+
+
+            // Is the fragment depth greater than what is in the shadow map?
+            if(shadowPosition.z > depthFromShadowMap) {
+                color = ambientComponent;
+            }
+        } // if(shadowPosition is in shadow map bounds)
+    } // if(shadowing)
+
+    // The output is a fragment with color and the correct transparency 'alpha'
+    outFragment.gl_FragColor = glm::vec4(glm::clamp(color, 0.f, 1.f), diffuseColor.a);
+} // student_drawModel_fragmentShader()
 //! [drawModel_fs]
 
+/*** end of file prepareModel.cpp ***/
--- studentSolution/src/studentSolution/prepareModel.hpp
+++ studentSolution/src/studentSolution/prepareModel.hpp
@@ -1,15 +1,125 @@
 /*!
- * @file
- * @brief This file contains function declarations for model rendering
+ * @file prepareModel.hpp
+ * @brief This file contains function declarations for model rendering.
  *
  * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Jan kalina, xkalinj00@stud.fit.vutbr.cz
  */
 #pragma once
 
 #include <solutionInterface/modelFwd.hpp>
 
-void student_prepareModel(GPUMemory&mem,CommandBuffer&commandBuffer,Model const&model);
+/*
+ * DISCLAIMER: This header file prototype documentation was co-created with the
+ *             help of GitHub Copilot as the header file documentation is not
+ *             part of the solution or a mandatory requirement. I only prepared
+ *             this documentation to keep the beautiful assignment documentation
+ *             consistent and so it contains my function if generated anew.
+ *             Other parts of the code were written by me, Jan Kalina.
+ */
 
-void student_drawModel_vertexShader(OutVertex&outVertex,InVertex const&inVertex,ShaderInterface const&si);
+/******************************************************************************/
+/*                                                                            */
+/*                PREPARE MODEL strongly based on pseudo-code                 */
+/*                                                                            */
+/******************************************************************************/
 
-void student_drawModel_fragmentShader(OutFragment&outFragment,InFragment const&inFragment,ShaderInterface const&si);
+/**
+ * @brief This function prepares model into memory and creates command buffer.
+ *
+ * @details This function initializes GPU memory with model data and prepares
+ *          a command buffer for rendering. It copies buffers and textures to
+ *          GPU memory, then recursively processes the model hierarchy starting
+ *          from root nodes. For each node with a valid mesh, appropriate vertex
+ *          arrays are created and rendering commands are added to the command
+ *          buffer. Transformation matrices are calculated to properly position
+ *          objects in the scene.
+ *
+ * @param mem GPU memory where resources and vertex arrays will be stored.
+ * @param commandBuffer Buffer that will be filled with rendering commands.
+ * @param model The 3D model containing hierarchical scene data with meshes and materials.
+ */
+void student_prepareModel(GPUMemory &mem, CommandBuffer &commandBuffer, const Model &model);
+
+/**
+ * @brief Recursively processes nodes in the model hierarchy.
+ *
+ * @details This helper function traverses the scene graph and processes each
+ *          node with a mesh for rendering. It creates vertex array objects
+ *          with appropriate attributes, adds commands to the command buffer
+ *          for rendering, and prepares uniform data including transformation
+ *          matrices. The function handles transformation inheritance through
+ *          the hierarchy by combining parent and local transformation matrices.
+ *          For each node with a valid mesh, it creates the necessary rendering
+ *          setup and then recursively processes all child nodes.
+ *
+ * @param memory GPU memory where vertex arrays and uniforms will be stored.
+ * @param commandBuffer Buffer where rendering commands will be stored.
+ * @param model The 3D model containing meshes and materials.
+ * @param node Current node being processed in the scene hierarchy.
+ * @param parentMatrix Accumulated transformation matrix from parent nodes.
+ * @param drawCounter Counter for draw commands used for uniform locations.
+ * @param vertexArrayCounter Counter for vertex array objects in memory.
+ */
+void prepareNode(GPUMemory &memory, CommandBuffer &commandBuffer, const Model &model,
+                 const Node &node, const glm::mat4 &parentMatrix, uint32_t &drawCounter,
+                 uint32_t &vertexArrayCounter);
+
+
+/******************************************************************************/
+/*                                                                            */
+/*       VERTEX SHADER strongly based on description in the assignment,       */
+/*                   which I followed almost to the letter                    */
+/*                                                                            */
+/******************************************************************************/
+
+/**
+ * @brief This function represents vertex shader of texture rendering method.
+ *
+ * @details Transforms vertices from model space to world space using
+ *          transformation matrices. Takes vertex attributes (position, normal,
+ *          texture coordinates) in model space and outputs transformed
+ *          attributes in world space along with light clip-space position for
+ *          shadow mapping. The shader applies model matrix for positions,
+ *          inverse transpose matrix for normals, and calculates final position
+ *          in both camera and light clip spaces using respective projection-view
+ *          matrices.
+ *
+ * @param outVertex Output vertex with transformed world-space position, normal,
+ *                  texture coordinates, and light-space position.
+ * @param inVertex Input vertex with model-space position, normal, and texture
+ *                 coordinates.
+ * @param si Shader interface with uniform variables including transformation
+ *           matrices.
+ */
+void student_drawModel_vertexShader(OutVertex &outVertex, const InVertex &inVertex,
+                                    const ShaderInterface &si);
+
+
+/******************************************************************************/
+/*                                                                            */
+/*      FRAGMENT SHADER strongly based on description in the assignment,      */
+/*                   which I followed almost to the letter                    */
+/*                                                                            */
+/******************************************************************************/
+
+/**
+ * @brief This functionrepresents fragment shader of texture rendering method.
+ *
+* @details Calculates fragment colors using Lambert illumination model with
+*          shadow mapping. Processes world-space positions and normals, applies
+*          texturing when available, handles double-sided surfaces, and computes
+*          shadows by comparing fragment depth with shadow map values. The final
+*          color combines ambient lighting with diffuse lighting affected by
+*          shadows. Alpha transparency is preserved from the material color.
+ *
+ * @param outFragment Output fragment with calculated color and alpha.
+ * @param inFragment Input fragment with world-space position, normal, texture
+ *                   coordinates, and light-space position.
+ * @param si Shader interface with uniform variables for lighting parameters
+ *           and textures.
+ */
+void student_drawModel_fragmentShader(OutFragment &outFragment, const InFragment &inFragment,
+                                      const ShaderInterface &si);
+
+/*** end of file prepareModel.hpp ***/
